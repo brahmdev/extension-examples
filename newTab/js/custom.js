0050d005f901f530803bbbade4bd351ab82029ce
brahmdev <pandey.dev25@gmail.com>
2018-12-10 00:13:40 +0100

    modal added for google shortcut and corresponding images added

--- newTab/js/custom.js
+++ newTab/js/custom.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-        chrome.management.getAll(getAllCallback);
-      });
+        //chrome.management.getAll(getAllCallback);
+		getAllGoogleServices();
+ });
 	  
 $(document).ready(function () {
 	$('#sidebarCollapse').on('click', function () {
 		$('#sidebar').toggleClass('active');
 	});
+	
+	$('#apps-menu').on('click', function () {
+		$('#apps-modal').modal('toggle');
+	});
+	
+	//console.log("**************** ",cjgShortcuts.mostPopular);
+	//console.log("List**********	 ",cjgShortcuts.list["59"]);
 });
 
 var getAllCallback = function(list) {
@@ -13,12 +21,11 @@ var getAllCallback = function(list) {
   for(var i in list) {
     // we don't want to do anything with extensions yet.
     var extInf = list[i];
-    if(extInf.isApp && extInf.enabled) {
       var app = document.createElement("div");
-
+ if(extInf.isApp && extInf.enabled) {
       var img = new Image();
       img.className = "image";
-      img.src = find128Image(extInf.icons);
+      img.src = find128Image(extInf.icon);
       img.setAttribute("title", extInf.name);   
       img.addEventListener("click", (function(ext) {
 	    return function() {
@@ -33,30 +40,38 @@ var getAllCallback = function(list) {
       app.appendChild(img);
       app.appendChild(name);
       apps.appendChild(app);
-    }
   }
- 
-/*	var img = new Image();
-      img.className = "image";
-      img.src = "/swagger.png";
-      img.setAttribute("style", "cursor:pointer");
-      img.setAttribute("title", extInf.name);
-      img.addEventListener("click", (function(ext) {
-            return function() {
-                window.open("https://inspector.swagger.io", "_blank");
-            };
-        })(extInf));
+}
+};
 
+var getAllGoogleServices = function() {
+  var services = document.getElementById("google-services");
+  for(var i in cjgShortcuts.mostPopular) {
+    // we don't want to do anything with extensions yet.
+    var extInf = cjgShortcuts.list[i];
+    if(extInf !== undefined) {
+      var service = document.createElement("div");
+
+      var img = new Image();
+      img.className = "image";
+	  //console.log(extInf.name , " : ", extInf.icon);
+      img.src = "images/48/" + extInf.icon + "_48dp.svg";
+      img.setAttribute("title", extInf.name);   
+      img.addEventListener("click", (function(ext) {
+	    return function() {
+	        window.location.href = extInf.u;
+	    };
+	})(extInf));
 
       var name = document.createElement("div");
       name.className = "name";
-      name.textContent = 'Swagger Inspector';
-      name.setAttribute("style", "display:block;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;width:125px;text-align:center;");
-      app.className = "app";
-      app.appendChild(img);
-      app.appendChild(name);
-      apps.appendChild(app);
-*/
+      name.textContent = extInf.name;
+      service.className = "service";
+      service.appendChild(img);
+      service.appendChild(name);
+      services.appendChild(service);
+    }
+  }
 };
 
 var find128Image = function(icons) {
