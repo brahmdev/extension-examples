460ff479460511445ca7376a4b2b234ed30cfef3
brahmdev <pandey.dev25@gmail.com>
2018-12-15 08:14:13 +0100

    sample datatable added in rules modal

--- newTab/js/custom.js
+++ newTab/js/custom.js
@@ -11,8 +11,46 @@ $(document).ready(function () {
 		$('#apps-modal').modal('toggle');
 	});
 	
+	$('#my-rules').on('click', function () {
+    $('#rules-modal').modal('toggle');
+    
+    var table = $('#example').DataTable( {
+      "ajax": "../data.json",
+      "columns": [
+          {
+              "className":      'details-control',
+              "orderable":      false,
+              "data":           null,
+              "defaultContent": ''
+          },
+          { "data": "name" },
+          { "data": "position" },
+          { "data": "office" },
+          { "data": "salary" }
+      ],
+      "order": [[1, 'asc']]
+  } );
+   
+  // Add event listener for opening and closing details
+  $('#example tbody').on('click', 'td.details-control', function () {
+      var tr = $(this).closest('tr');
+      var row = table.row( tr );
+
+      if ( row.child.isShown() ) {
+          // This row is already open - close it
+          row.child.hide();
+          tr.removeClass('shown');
+      }
+      else {
+          // Open this row
+          row.child( format(row.data()) ).show();
+          tr.addClass('shown');
+      }
+   } );
+});
 	
-    $(".container-fluid").load("../apps-modal.html"); 
+  $(".apps-modal").load("../apps-modal.html"); 
+	$(".rules-modal").load("../rules-modal.html"); 
 	
 	chrome.management.getAll(getAllCallback);
 });
